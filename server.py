496a523f2512a76328875fea72fa6fd69d6bb670
Julien <julien@goliaths.io>
2024-05-24 19:17:57 +0200

    added .env

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 from flask import Flask, request, jsonify
-from entry import *  # Import your function from main.py
+from entry import *
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+# Load .env
+load_dotenv()
+base_dir = os.getenv("BASE_TEST_DIR")
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app)
 
 @app.route('/run-script', methods=['GET'])
 def run_script():
-  folder = request.args.get('folder', default='', type=str)
+  folder = base_dir #request.args.get('folder', default='', type=str)
   prompt = request.args.get('prompt', default='', type=str)
   filePaths = request.args.get('filePaths', default='', type=str)
   
@@ -15,6 +21,7 @@ def run_script():
     entry(folder, prompt, filePaths)
     return jsonify(str("ok")), 200
   except Exception as e:
+    print(e)
     return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
