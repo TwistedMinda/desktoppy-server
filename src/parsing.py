5446ff8c69e2f7424e96afc168fffd26a4775c80
Julien Brunet <julien.brunet.92@gmail.com>
2024-05-25 08:13:37 +0200

    Adding history helps logic a lot

--- src/parsing.py
+++ src/parsing.py
@@ -11,16 +11,17 @@ def add_parser_instructions(prompt: str, directory: str, history: str = ""):
   filenames = get_all_filenames(directory)
   detailed_prompt = (
     f"{format_history(history)}"
-    f"You are an exceptional and forgiving parser. Your are here to detect any action that has to be taken on files. \n"
+    f"You are an exceptional and forgiving parser. Your are here to detect any action that has to be taken on files, if user is not talking about files just respond a empty array. \n"
     f"Respond only with a valid JSON. Double-check its validity meticulously. Do not include any code blocks. Your response should be like a real API. \n"
-    f"Response must be array of objects, each with:'\n"
+    f"IMPORTANT: Simply return an empty array if no action is found, it's perfectly fine.\n"
+    f"ONLY IF ACTIONS WHERE FOUND (no empty action allowed in the array), return array of objects, each with:'\n"
+    f"- action: MUST BE one of ['read','create', 'delete', 'modify', 'copy', 'rename'] otherwise it is NOT an action \n"
     f"- filePath: The path of the file to be read, created, modified or deleted, moved or copied. \n"
     f"- query: The action that will be forwarded to another AI Agent so it must be as detailed as possible\n"
-    f"- action: one of ['read','create', 'delete', 'modify', 'copy', 'rename'] \n"
     f"Other rules: \n"
     f"- User folder base path is {directory}. \n"
+    f"- Don't always try to read a file when user is asking a question, only do if you are sure the user refers to a specific file"
     f"- Accessible files for reference: {','.join(filenames)}. \n"
-    f"IMPORTANT: Simply return an empty array if no action is found, it's perfectly fine.\n"
     f"User request: {prompt}"
 )
   print("_______Instructions________")
